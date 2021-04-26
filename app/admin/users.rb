f750352f93650e0d275c5dfac309e4e92dabfaf2
Krista Stadler <stadler.krista@gmail.com>
2021-04-26 15:12:05 -0600

    Create users.rb
    Added users to ActiveAdmin

---
+++ app/admin/users.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :business_name, :email_address, :password_digest, :role
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :business_name, :email_address, :password_digest, :role]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end

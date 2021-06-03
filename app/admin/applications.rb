709847d4f67edae9ef8fca11c93e64648aa9f090
“Krista <stadler.krista@gmail.com>
2021-06-02 19:51:43 -0600

    fixed copy and updated activeadmin

--- app/admin/applications.rb
+++ app/admin/applications.rb
@@ -1,5 +1,8 @@
  ActiveAdmin.register Application do
-
+   actions :index, :show, :edit, :update
+   permit_params :website, :facebook_link, :instagram_link, :short_description, :category, :mlm_status, :description, :first_event,
+                 :best_gift, :price_range, :booth_choice, :previous_vendor, :previous_efforts, :image_one, :image_two, :additional_info,
+                 :event_id, :user_id
     # See permitted parameters documentation:
     # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
     #
@@ -16,5 +19,3 @@
     #   permitted
     # end
 end
-
-
--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Event do
-
+  
+  actions :index, :show, :edit, :update
   permit_params :name, :application_open_date, :application_close_date
 
 end

d140742b42261cc5adae3c81cbdebe035a7fd8d2
“Krista <stadler.krista@gmail.com>
2021-06-02 19:52:13 -0600

    updated active admin again

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Event do
-  
-  actions :index, :show, :edit, :update
+
+  actions :index, :show, :edit, :update, :delete
   permit_params :name, :application_open_date, :application_close_date
 
 end
